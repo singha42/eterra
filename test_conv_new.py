3599219ce7f24f6c52ec03913b4dd85879fa35f4
singha42 <43796015+singha42@users.noreply.github.com>
2019-03-26 10:44:12 -0700

    Add files via upload

--- test_conv_new.py
+++ test_conv_new.py
@@ -3,19 +3,20 @@ import json
 out_json=list()
 
 
-{attribute_map :[
-"capacitors"[
-{"attribute" : ["RegulatingControl.mode","RegulatingControl.targetDeadband","RegulatingControl.targetValue","ShuntCompensator.aVRDelay","ShuntCompensator.sections"]}
-]
-"switches"[
-{"attribute" : "Switch.open"}
-]
+attribute_map = {
+    "capacitors" : {
+        "attribute" : ["RegulatingControl.mode","RegulatingControl.targetDeadband","RegulatingControl.targetValue","ShuntCompensator.aVRDelay","ShuntCompensator.sections"]}
+    ,
+    "switches": {
+        "attribute" : "Switch.open"
+     }
+    ,
 
-"regulator"[
-{"attribute" : ["RegulatingControl.targetDeadband","RegulatingControl.targetValue", "TapChanger.initialDelay", "TapChanger.step", "TapChanger.lineDropCompensation", "TapChanger.lineDropR","TapChanger.lineDropX"]}
-]]
+    "regulators" : {
+        "attribute" : ["RegulatingControl.targetDeadband","RegulatingControl.targetValue", "TapChanger.initialDelay", "TapChanger.lineDropCompensation","TapChanger.step", "TapChanger.lineDropR","TapChanger.lineDropX"]}
+
 }
-		
+
 class dnp3_mapping():
     def __init__(self,map_file):
 	self.c_ao = 0
@@ -40,7 +41,7 @@ class dnp3_mapping():
         self.out_json.append(records)
 
 
-    def assign_valc(self,data_type,group,variation,index,name,description,measurement_type,measurement_id,attribute):
+    def assign_valc(self,data_type,group,variation,index,name,description,object_id,attribute):
 	records = dict()
 	records["data_type"] = data_type
 	records["index"] = index
@@ -48,8 +49,7 @@ class dnp3_mapping():
 	records["variation"] = variation
 	records["description"] = description
 	records["name"] = name
-	records["measurement_type"] = measurement_type
-	records["measurement_id"] = measurement_id
+	records["object_id"] = object_id
 	records["attribute"] = attribute
 	self.out_json.append(records)
 
@@ -67,7 +67,7 @@ class dnp3_mapping():
 	    switches = x.get("switches",[])
 	    solarpanels = x.get("solarpanels",[])
 	    batteries = x.get("batteries",[])
-				
+
 	for m in measurements:
 	    measurement_type = m.get("measurementType")
 	    measurement_id = m.get("mRID")
@@ -80,21 +80,21 @@ class dnp3_mapping():
 	    elif m['MeasurementClass'] == "Discrete":
 		self.assign_val("BO",11,1,self.c_bo,name,description,measurement_type,measurement_id)
 		self.c_bo += 1
-		
+
 	for m in capacitors:
-	    measurement_id = m.get("mRID")
-	    name =  m.get("name")
-#            print(len(m['phases']))
-            for j in range(0, len(m['phases'])):
-                phase_value = list(m['phases'])
-                description = "Capacitor, " + m['name'] + "," + "phase -" + phase_value[j]
-	        if "targetDeadband" in capacitors:
-	            self.assign_valc("AI",32,3,self.c_ai,name,description,measurement_type,measurement_id,None)
-	            self.c_ai += 1
-                else:
-		    self.assign_valc("BI",2,1,self.c_bi,name,description,None,measurement_id,None)
-    	            self.c_bi +=1
-		
+            object_id = m.get("mRID")
+    	    name =  m.get("name")
+	    phase_value = list(m['phases'])
+	    description1 = "Capacitor, " + m['name'] + "," + "phase -" + m['phases']
+            cap_attribute = attribute_map['capacitors']['attribute']
+	    for l in range(0,4):
+		self.assign_valc("AI",32,3,self.c_ai,name,description1,object_id,attribute_map['capacitors']['attribute'][l])
+		self.c_ai += 1
+	    for j in range(0, len(m['phases'])):
+		description = "Capacitor, " + m['name'] + "," + "phase -" + phase_value[j]
+		self.assign_valc("BI",2,1,self.c_bi,name,description,object_id,attribute_map['capacitors']['attribute'][4])
+		self.c_bi +=1
+
         for m in solarpanels:
             measurement_id = m.get("mRID")
             name = m.get("name")
@@ -109,24 +109,32 @@ class dnp3_mapping():
             self.assign_val("AI",32,3,self.c_ai,name,description,None,measurement_id)
             self.c_ai +=1    
             
-        
         for m in switches:
-	    measurement_id = m.get("mRID")
+	    object_id = m.get("mRID")
 	    name = m.get("name")
-	    attribute = m.get("attribute")
 	    for k in range(0, len(m['phases'])):
-		phase_value = list(m['phases'])
-		description = "Switch, " + m['name'] + "phases - " + phase_value[k]
-	  	self.assign_val("BI",2,1,self.c_bi,name,description,None,measurement_id,attribute)
-	    	self.c_bi +=1
+	        phase_value = list(m['phases'])
+                description = "Switch, " + m['name'] + "phases - " + phase_value[k]
+	  	self.assign_valc("BI",2,1,self.c_bi,name,description,object_id,attribute_map['switches']['attribute'])
+	        self.c_bi +=1
 			
-	for m in regulators:
+        for m in regulators:
 	    name = m.get("bankName")
-	    for i in range(int(m['size'])):
-    	        measurement_id = m.get("mRID")[i]
-		description = "Regulator, " + m['tankName'][i] + " " "phase is  -  " + m['endPhase'][i]
-		self.assign_val("AI",32,3,self.c_ai,name,description,None,measurement_id)
-	        self.c_ai +=1
+	    reg_attribute = attribute_map['regulators']['attribute']
+            bank_phase= list(m['bankPhases'])
+            description = "Regulator, " + m['bankName'] + " " +"phase is  -  " + m['bankPhases']
+	    for n in range(0,5):
+	        object_id = m.get("mRID")
+		self.assign_valc("AI",32,3,self.c_ai,name,description,object_id[0],reg_attribute[n])
+		self.c_ai +=1
+            for i in range(4,7):
+	        reg_phase_attribute=attribute_map['regulators']['attribute'][i]
+	        for j in range(0, len(m['bankPhases'])):
+		    description = "Regulator, " + m['tankName'][j] + " " "phase is  -  " + m['bankPhases'][j]
+	            object_id = m.get("mRID")
+	            self.assign_valc("AI",32,3,self.c_ai,name,description,object_id[j],reg_phase_attribute)
+		    self.c_ai +=1
+
 	
 
 	return self.out_json
